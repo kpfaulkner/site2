88be2da0f269fbb2d7030a6116a6adcea551516f
Ken Faulkner <ken.faulkner@gmail.com>
2013-04-21 16:42:39 +1000

    adding storage class

---
+++ site2/DataManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace site2
+{
+    class DataManager
+    {
+        Windows.Storage.ApplicationDataContainer localSettings;
+        StorageFolder localFolder;
+        
+        public DataManager()
+        {
+            localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+            localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+        }
+
+        public async void WriteData( List<WordEntry> wordList)
+        {
+            StorageFile dataFile = await localFolder.CreateFileAsync("wordfile.txt",CreationCollisionOption.ReplaceExisting);
+
+            var json = JsonHelper.SerializeObjectToJson<List<WordEntry>>(wordList);
+
+            await FileIO.WriteTextAsync(dataFile, json);
+
+        }
+
+        async void ReadData()
+        {
+            try
+            {
+                StorageFile dataFile = await localFolder.GetFileAsync("wordfile.txt");
+                String json = await FileIO.ReadTextAsync(dataFile);
+
+                var wordList = JsonHelper.DeserializeJsonToObject<List<WordEntry>>(json);
+                //return wordList;
+            }
+            catch (Exception)
+            {
+                // Timestamp not found
+            }
+        }
+
+    }
+}
